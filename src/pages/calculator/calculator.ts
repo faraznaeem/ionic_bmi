e1aacf394e84cf949eaa1eae37a574baa34960a0
faraznaeem <faraznaeem@gmail.com>
2017-08-08 13:16:57 +0200

    add logic and view

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -14,9 +14,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'calculator.html',
 })
 export class CalculatorPage {
+  height: number;
+  weight: number;
+  bmiValue: number;
+  bmiMessage: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
+  calculateBMI(){
+    if (this.weight > 0 && this.height > 0){
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+private setBMIMessage() {
+  if (this.bmiValue < 18.5) {
+    this.bmiMessage = "You are a skinny devil"
+  }
+
+  if (this.bmiValue > 18.5 && this.bmiValue < 25){
+    this.bmiMessage = "Average Joe"
+  }
+
+  if (this.bmiValue > 25 && this.bmiValue < 30) {
+    this.bmiMessage = "Fatty goes boom boom"
+  }
+
+  if (this.bmiValue > 30) {
+    this.bmiMessage = "Call 911 we have a whale here"
+  }
+}
+
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CalculatorPage');
